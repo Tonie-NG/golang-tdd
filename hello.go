30dcacd45200fa61973e2098c8e03640208fb393
Tonie <victornwanochi@gmail.com>
2023-07-13 11:11:45 +0100

    Completed hello world test

--- hello.go
+++ hello.go
@@ -2,10 +2,31 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return "Hello, " + name
+const spanish = "Spanish"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const french = "French"
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	prefix := englishHelloPrefix
+
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+
+	case spanish:
+		prefix = spanishHelloPrefix
+		
+	}
+
+	return prefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
