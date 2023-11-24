b0db0e1088d866e10848e3967161327920ce05ea
Tonie Victor <victornwanochi@gmail.com>
2023-11-24 06:49:34 +0100

    dependency injection

--- structs-mi/shapes_test.go
+++ structs-mi/shapes_test.go
@@ -21,7 +21,7 @@ func TestArea(t *testing.T) {
     want float64
   } {
     {name: "Rectangle", shape: Rectangle{12, 6}, want: 72.0},
-    {name: "Circle",shape: Circle{10}, want: 314.1592653589793},
+    {name: "Circle", shape: Circle{10}, want: 314.1592653589793},
     {name: "Triangle", shape: Triangle{12, 6}, want: 36.0},
   }
 
